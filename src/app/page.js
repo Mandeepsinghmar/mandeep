ef3b17c0d0f8a84e651f60aaa1b03cccfc796d46
Mandeep Singhmar <msingh8328@gmail.com>
2025-03-29 13:24:38 +0530

    fix: contact, projects and skills

--- src/app/page.js
+++ src/app/page.js
@@ -6,10 +6,10 @@ import { GeistMono } from 'geist/font/mono';
 export default function Home() {
   return (
     <div
-      className={`min-h-screen bg-[#ebebeb] text-[#1a1a1a] ${GeistMono.className}`}
+      className={`min-h-screen bg-[#FEFCF7] text-[#1a1a1a] ${GeistMono.className}`}
     >
       {/* Hero Section */}
-      <header className='container mx-auto px-4 py-20 max-w-4xl'>
+      <header className='container mx-auto px-4 py-18 max-w-4xl'>
         <div className='flex flex-col gap-10'>
           <div className='flex items-center gap-8'>
             <div className='w-28 h-28 rounded-2xl bg-gradient-to-r from-violet-600 to-indigo-600 flex items-center justify-center text-3xl font-bold text-white shadow-lg shadow-violet-600/20'>
@@ -33,15 +33,29 @@ export default function Home() {
               I've delivered multiple successful projects, focusing on creating
               performant and beautiful user interfaces. When not coding, I enjoy
               exploring new technologies and contributing to the developer
-              community.
+              community. Hey, I'm Sanidhya, a full-stack developer specializing
+              in building MVPs, landing pages, multi-page websites, and SaaS
+              solutions. I thrive on transforming complex ideas into seamless
+              web applications and am always eager to learn and tackle new
+              challenges. With a strong commitment to delivering high-quality
+              work, I ensure that projects are completed efficiently and
+              effectively. So basically, I can get work done :)
             </p>
             <p className='text-gray-700 text-lg leading-relaxed'>
               Available for work, freelance projects, and collaborations.{' '}
               <Link
                 href='mailto:msingh8328@gmail.com'
-                className=' text-violet-600 hover:text-violet-700 font-medium'
+                className=' text-violet-600 hover:text-violet-700 font-medium underline'
               >
-                contact me
+                email
+              </Link>{' '}
+              or{' '}
+              <Link
+                href='https://x.com/mandeep_singh_9'
+                target='_blank'
+                className=' text-violet-600 hover:text-violet-700 font-medium underline'
+              >
+                x
               </Link>
             </p>
           </div>
@@ -51,7 +65,7 @@ export default function Home() {
       {/* Work Experience */}
       <section className='py-12 '>
         <div className='container mx-auto px-4 max-w-4xl'>
-          <h2 className='text-2xl font-bold mb-12 text-black'>
+          <h2 className='text-2xl font-bold mb-6 text-black'>
             Work Experience
           </h2>
           <div className='space-y-6'>
@@ -87,7 +101,7 @@ export default function Home() {
       {/* Tools & Skills */}
       <section className='py-12 '>
         <div className='container mx-auto px-4 max-w-4xl'>
-          <h2 className='text-2xl font-bold mb-12 text-black'>
+          <h2 className='text-2xl font-bold mb-6 text-black'>
             Tools & technologies
           </h2>
           <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
@@ -97,17 +111,17 @@ export default function Home() {
               'TypeScript',
               'JavaScript',
               'Tailwind CSS',
-              'Material-UI',
               'Node.js',
               'Express',
               'Redux',
+              'MERN',
+              'Material-UI',
               'Firebase',
               'Git',
-              'MERN Stack',
             ].map((tool) => (
               <div
                 key={tool}
-                className='p-4 bg-[#f3f2f2] rounded-xl text-center hover:bg-[#f8f8f8] transition-all hover:shadow-lg hover:shadow-violet-600/5 border border-gray-100'
+                className='p-4  rounded-xl text-center hover:bg-[#f5f5f2] transition-all hover:shadow-lg hover:shadow-violet-600/5 border border-gray-100'
               >
                 <span className='text-gray-600 text-sm'>{tool}</span>
               </div>
@@ -119,84 +133,71 @@ export default function Home() {
       {/* Featured Projects */}
       <section className='py-12'>
         <div className='container mx-auto px-4 max-w-4xl'>
-          <h2 className='text-2xl font-bold mb-12 text-black'>
-            Featured Projects
-          </h2>
+          <h2 className='text-2xl font-bold mb-6 text-black'>Projects</h2>
           <div className='grid md:grid-cols-2 gap-6'>
             <ProjectCard
-              title='Financial Platform'
-              description="Working on Softhesis Labs' financial platform, implementing modern UI components and integrating complex financial data visualization."
-              tech={['Next.js', 'TypeScript', 'Tailwind CSS', 'Redux']}
-              link='https://tryfinancially.com'
+              title='Lyriks'
+              description='Lyriks is a modern music streaming web application that allows users to explore and play their favorite songs with an intuitive and engaging user interface.'
+              tech={['React', 'Tailwind CSS', 'Redux']}
+              link='https://lyriks.vercel.app'
+              github='https://github.com/Mandeepsinghmar/lyriks/tree/main'
+            />
+
+            <ProjectCard
+              title='Tiktik'
+              description='A TikTok-like social media application built using Next.js, React, zustand and Sanity as the backend.'
+              tech={['Next.js', 'Tailwind CSS', 'Redux', 'Typescript']}
+              link='https://tiktok-jsm.vercel.app/'
+              github='https://github.com/Mandeepsinghmar/tiktok-clone'
             />
             <ProjectCard
-              title='E-learning Platform'
-              description='Built over 20 educational projects for JavaScript Mastery, helping thousands of students learn modern web development technologies.'
-              tech={['React', 'Node.js', 'MongoDB', 'Express']}
-              link='#'
-            />
-            <ProjectCard
-              title='Streaming Platform'
-              description='Developed the frontend interface for Eagle3Dstreaming, implementing real-time video streaming features and responsive design.'
-              tech={['React', 'WebRTC', 'Material-UI', 'Redux']}
-              link='https://www.eagle3dstreaming.com'
-            />
-            <ProjectCard
-              title='Health & Nutrition Platform'
+              title='Examine'
               description='Led the frontend development for Examine.com, building a modern and user-friendly interface for accessing health and nutrition information.'
-              tech={['Next.js', 'GraphQL', 'Styled Components', 'Redux']}
+              tech={['Next.js', 'Tailwind CSS', 'Redux']}
               link='https://examine.com'
+            />
+            <ProjectCard
+              title='More projects'
+              description='Built over 20+ educational projects for JavaScript Mastery, helping thousands of students learn modern web development technologies.'
+              tech={['React', 'Node.js', 'MongoDB', 'Express', 'etc.']}
+              link='https://www.youtube.com/playlist?list=PL6QREj8te1P6wX9m5KnicnDVEucbOPsqR'
             />
           </div>
         </div>
       </section>
 
       {/* Contact */}
-      <section className='py-12'>
-        <div className='container mx-auto px-4 max-w-4xl text-center'>
-          <h2 className='text-2xl font-bold mb-8 text-black'>Get in Touch</h2>
-          <p className='text-gray-700 mb-8 max-w-2xl mx-auto text-lg'>
-            I'm currently available for remote frontend development
-            opportunities. Feel free to reach out if you'd like to work
-            together!
-          </p>
-          <div className='flex justify-center gap-6'>
-            <Link
-              href='mailto:msingh8328@gmail.com'
-              className='px-6 py-3 bg-violet-600 hover:bg-violet-700 rounded-xl transition-all hover:shadow-lg hover:shadow-violet-600/20 text-white font-medium'
-            >
-              Contact Me
-            </Link>
-            <Link
-              href='#'
-              className='px-6 py-3 bg-[#f3f2f2] border border-gray-200 hover:border-violet-200 rounded-xl transition-all hover:shadow-lg hover:shadow-violet-600/10 font-medium text-gray-700 hover:text-violet-600'
-            >
-              View Resume
-            </Link>
+      <section className='pt-8 pb-48'>
+        <div className='container mx-auto px- max-w-4xl '>
+          <h2 className='text-2xl font-bold mb-4 text-black'>Reach me on</h2>
+
+          <div className='flex items-center'>
+            <div className='flex justify-center gap-8'>
+              <SocialLink
+                href='https://github.com/Mandeepsinghmar'
+                label='github'
+              />
+              <SocialLink
+                href='https://www.linkedin.com/in/mandeep-singhmar/'
+                label='linkedin'
+              />
+              <SocialLink href='https://x.com/mandeep_singh_9' label='x' />
+              <SocialLink href='mailto:msingh8328@gmail.com' label='email' />
+              <SocialLink
+                href='https://drive.google.com/file/d/1Nadw3zINPVSSKKQjRLMrORtnT62wFUaC/view?usp=sharing'
+                label='resume'
+              />
+            </div>
           </div>
         </div>
       </section>
-
-      {/* Footer */}
-      <footer className='py-12 border-t border-gray-200'>
-        <div className='container mx-auto px-4 max-w-4xl'>
-          <div className='flex justify-center gap-8'>
-            <SocialLink href='#' label='GitHub' />
-            <SocialLink href='#' label='LinkedIn' />
-            <SocialLink href='#' label='Twitter' />
-          </div>
-          <p className='text-center text-gray-500 mt-8'>
-            © 2025 Mandeep Singhmar. All rights reserved.
-          </p>
-        </div>
-      </footer>
     </div>
   );
 }
 
-function ProjectCard({ title, description, tech, link }) {
+function ProjectCard({ title, description, tech, link, github }) {
   return (
-    <div className='p-6 bg-[#f3f2f2] rounded-xl hover:bg-[#f8f8f8] transition-all hover:shadow-lg hover:shadow-violet-600/10 border border-gray-100 group'>
+    <div className='p-6  rounded-xl hover:bg-[#f5f5f2] transition-all hover:shadow-lg hover:shadow-violet-600/10 border border-gray-100 '>
       <h3 className='text-xl font-bold mb-3 text-gray-900'>{title}</h3>
       <p className='text-gray-600 mb-4 leading-relaxed'>{description}</p>
       <div className='flex flex-wrap gap-2 mb-4'>
@@ -209,23 +210,35 @@ function ProjectCard({ title, description, tech, link }) {
           </span>
         ))}
       </div>
-      <Link
-        href={link}
-        className='text-violet-600 hover:text-violet-700 flex items-center gap-2 group-hover:gap-3 transition-all font-medium'
-        target='_blank'
-      >
-        View Project
-        <span className='text-sm'>→</span>
-      </Link>
+      <div className='flex gap-4'>
+        <Link
+          href={link}
+          className='text-violet-600 hover:text-violet-700 flex items-center hover:underline gap-2  transition-all font-medium'
+          target='_blank'
+        >
+          View Project
+          <span className='text-sm'>→</span>
+        </Link>
+        {github && (
+          <Link
+            href={github}
+            className='text-violet-600 hover:text-violet-700 flex items-center gap-2 hover:underline  transition-all font-medium'
+            target='_blank'
+          >
+            github
+            <span className='text-sm'>→</span>
+          </Link>
+        )}
+      </div>
     </div>
   );
 }
 
 function ExperienceCard({ title, company, date, description }) {
   return (
-    <div className='p-6 bg-[#f3f2f2] rounded-xl border border-gray-100 hover:shadow-lg hover:shadow-violet-600/10 hover:bg-[#f8f8f8] group transition-all'>
-      <h3 className='text-xl font-bold text-gray-900'>{title}</h3>
-      <p className='text-gray-700 mt-1'>{company}</p>
+    <div className='p-6  rounded-xl border border-gray-100 hover:shadow-lg hover:shadow-violet-600/10 hover:bg-[#f5f5f2] group transition-all'>
+      <h3 className='text-xl font-bold text-gray-900'>{company}</h3>
+      <p className='text-gray-700 mt-1'>{title}</p>
       <p className='text-sm text-gray-500 mt-1'>{date}</p>
       <p className='mt-4 text-gray-600 leading-relaxed'>{description}</p>
     </div>
@@ -236,7 +249,7 @@ function SocialLink({ href, label }) {
   return (
     <Link
       href={href}
-      className='text-gray-600 hover:text-violet-600 transition-colors font-medium'
+      className='text-gray-600 hover:text-violet-600 transition-colors font-medium underline'
       target='_blank'
     >
       {label}
